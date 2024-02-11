28d9ef704ded603b51daa51b5471d454426b18b2
jattinmanhas <63615545+jattinmanhas@users.noreply.github.com>
2024-02-11 19:36:02 +0530

    Completed whole react refresher along with advanced topics about react-router-dom

--- react-app/src/components/Modal.jsx
+++ react-app/src/components/Modal.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import classes from "./Modal.module.css"
+import { useNavigate } from 'react-router-dom';
 
-const Modal = ({children, onClose}) => {
+const Modal = ({children}) => {
+  const navigate = useNavigate();
+
+  function closeHandler (){
+    navigate('..');
+  }
+
   return (
     <>
-        <div className={classes.backdrop} onClick={onClose} />
+        <div className={classes.backdrop} onClick={closeHandler} />
         <dialog open className={classes.modal}>
             {children}
         </dialog>
--- react-app/src/components/Post.jsx
+++ react-app/src/components/Post.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 import classes from "./Post.module.css";
+import { Link } from "react-router-dom";
 
-const names = ["Jattin", "Manhas"]
+const names = ["Jattin", "Manhas"];
 
 const Post = (props) => {
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
 
   return (
     <div /*style={{color: 'white'}}*/ className={classes.post}>
+      <Link to={props.id}>
         <p className={classes.author}>{props.author}</p>
         <p className={classes.text}>{props.body}</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
